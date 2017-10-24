ae75aa40a744d9a8c175048fb14761ddb61c5415
thatcat13 <thatcat13@gmail.com>
2017-10-23 20:13:56 -0700

    get array to work by re-coding upperArray directions and adding .sort();

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#groceryList").submit(function(event) {
   event.preventDefault();
-  var groceries = ["groc1", "groc2", "groc"];
+  var groceries = ["groc1", "groc2", "groc3"];
   var arrayedGroceries = [];
   groceries.forEach(function(food) {
     var items = $("input#" + food).val();
@@ -9,11 +9,13 @@ $(document).ready(function() {
     });
 
   var upperArray = arrayedGroceries.map(function(stuff) {
-    return stuff.toUpperCase();
+    stuff= stuff.toUpperCase();
+    return stuff;
   });
 
+  upperArray.sort();
   upperArray.forEach(function(li) {
-    $("#theList").prepend("<li>" + li + "</li>");
+    $("#theList").append("<li>" + li + "</li>");
   });
 });
 });
